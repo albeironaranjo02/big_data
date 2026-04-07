445f452387c969d439fd4ee07c39d255c52f6136
albeironaranjo02 <114957702+albeironaranjo02@users.noreply.github.com>
2026-04-07 01:14:11 +0000

    comit universidad

--- notebooks/ejercicio4.py
+++ notebooks/ejercicio4.py
@@ -6,8 +6,8 @@ class MaximoPorCandidato(MRJob):
         partes = linea.strip().split(',')
         if len(partes) == 3:
             candidato = partes[0]
-            votos = int(partes[2])  # Cuidado: convertir texto a entero
-
+            votos = int(partes[2])  
+            # Cuidado: convertir texto a entero
             # INICIA TU CODIGO AQUI
             # Emite el candidato y los votos de esta acta especifica
             yield candidato, votos
